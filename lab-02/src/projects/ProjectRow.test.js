92cfe66c58906df6e8917755524c41f374ab942a
Ivan Kluzak <ivan.kluzak@constellationmutual.com>
2019-08-07 10:37:40 -0500

    Lab 2 completed successfully

--- lab-02/src/projects/ProjectRow.test.js
+++ lab-02/src/projects/ProjectRow.test.js
@@ -3,6 +3,28 @@ import { shallow } from 'enzyme';
 
 import ProjectRow from './ProjectRow';
 
+let wrapper;
+
 describe('<ProjectRow />', () => {
-  // TODO Add tests
+
+
+    beforeEach(() => {
+        const project = {
+          name: 'NAME',
+          description: 'DESCRIPTION'
+        };
+        wrapper = shallow(<ProjectRow project={project} />);
+      });
+
+      it('should instantiate the Project Row Component', () => {
+        expect(wrapper).toHaveLength(1);
+      });
+      
+      it('should render values into expected cells', () => {
+        expect(wrapper.find('td')).toHaveLength(2);
+        expect(wrapper.find('td').at(0).text()).toEqual('NAME');
+        expect(wrapper.find('td').at(1).text()).toEqual('DESCRIPTION');
+      });
+
+
 });
